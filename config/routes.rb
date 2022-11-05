9f90831ceaa7c283efdd5615c6a88d69782e7e61
tsuruta <naritakundono@gmail.com>
2022-11-05 08:32:26 +0000

    [Add]routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,30 @@ Rails.application.routes.draw do
     registrations: "public/registrations",
     sessions: "public/sessions"
   }
+
+  scope module: :public do
+    root to: "homes#top"
+    get "/about" => "homes#about", as: 'about'
+    resources :customers, only: [:show, :edit, :update] do
+      resource :relationships, only: [:create, :destroy]
+      get "followings" => "relationships#followings", as: "followings"
+      get "followers" => "relationships#followers", as: "followers"
+    end
+    resources :spots, only: [:index, :create] do
+      resources :posts, only: [:index, :show, :create, :destroy]
+    end
+    resources :posts, except: [:show, :new, :index, :edit, :create, :update, :destroy] do
+      resources :post_comments, only: [:create, :destroy]
+      resource :favorites, only: [:create, :destroy]
+    end
+    get "search" => "searches#search"
+  end
+
+  namespace :admin do
+    root to: "homes#top"
+    resources :customers, only: [:show, :edit, :update]
+    resources :spot, only: [:index, :destroy]
+  end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
