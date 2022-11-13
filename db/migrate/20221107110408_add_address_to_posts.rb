df6df6cab8ce4948e010971b07b07217e0f0f12c
tsuruta <naritakundono@gmail.com>
2022-11-13 10:02:12 +0000

    Fix

--- db/migrate/20221107110408_add_address_to_posts.rb
+++
@@ -1,6 +0,0 @@
-class AddAddressToPosts < ActiveRecord::Migration[6.1]
-  def change
-    add_column :posts, :address, :string
-    add_column :posts, :latitude, :float
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_11_08_043248) do
+ActiveRecord::Schema.define(version: 2022_11_13_100112) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -82,15 +82,13 @@ ActiveRecord::Schema.define(version: 2022_11_08_043248) do
   end
 
   create_table "posts", force: :cascade do |t|
+    t.integer "spot_id", null: false
     t.integer "customer_id", null: false
     t.text "body", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "address"
     t.string "spot_name"
-    t.float "latitude"
-    t.float "longitude"
-    t.integer "spot_id"
+    t.text "address"
   end
 
   create_table "relationships", force: :cascade do |t|
