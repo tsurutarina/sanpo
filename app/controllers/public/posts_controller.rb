3df5fe98795a0e0198c1dbe9b742d7ab31f7eaea
tsuruta <naritakundono@gmail.com>
2022-11-13 11:25:59 +0000

    test-deploy

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -36,6 +36,6 @@ class Public::PostsController < ApplicationController
 
   private
   def post_params
-    params.require(:post).permit(:spot_image, :address, :spot_name, :body)
+    params.require(:post).permit(:spot_image, :address_spot, :spot_name, :body)
   end
 end
