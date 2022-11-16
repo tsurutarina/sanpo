3ef492484cd920ffd0cf3469b05760bd5dcbdfda
tsuruta <naritakundono@gmail.com>
2022-11-16 03:57:51 +0000

    [Add]fav

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -35,8 +35,6 @@ class Public::SessionsController < Devise::SessionsController
   protected
 
   def customer_state
-
-
     @customer = Customer.find_by(email: params[:customer][:email])
     if @customer
       if @customer.valid_password?(params[:customer][:password]) && (@customer.is_deleted == true)
